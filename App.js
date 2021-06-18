e71dc5c9b2c0d3312c164c484c4b5cc3566e78ac
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-18 12:26:38 +0200

    Get name for textinput

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  // Variable / Ce qui va permettre de modifier la variable / Valeur par défaut
+  const [title, setTitle] = useState("Bonjour");
+  const [name, setName] = useState("");
+
+  function hello() {
+    // Bonjour -> Bijour lé gins
+    setTitle('Bijour lé gins');
+  }
+
+  function displayName() {
+    console.log(name);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{title}</Text>
+      <Text style={styles.textRoxor}>Voilà une application de Roxor!</Text>
+      <Button onPress={hello} title="Commencer à codey" color="blue" />
+
+
+      <View style={styles.formulairrr}>
+        <TextInput style={styles.input} value={name}
+         onChangeText={(text) => {setName(text)}} />
+        <Button title="Go!" color="red" onPress={displayName} />
+      </View>
+
     </View>
   );
 }
@@ -18,4 +39,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textRoxor: {
+    color: '#ff3d6a',
+    backgroundColor: '#003dff',
+    padding: 20,
+    marginBottom: 30
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: 'red',
+    margin: 5
+  },
+  formulairrr: {
+    flexDirection: 'row'
+  }
 });
